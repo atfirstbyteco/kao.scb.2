d876c37cacb959f250f47ea4b700a39a1f4b9ad8
AJENAJEN <i_hear_nie@hotmail.com>
2018-09-03 17:15:31 +0700

    fixed sponsor order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
         ])->pluck('total_donate');
         $sponsor = Sponsor::where([
 	        'sponsor_status' => 'active',
-        ])->get();
+        ])->orderBy('sponsor_id')->get();
 
         // $sponsor_row = $sponsor->chunk(20);
 
