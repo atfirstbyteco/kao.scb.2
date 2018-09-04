07d041530be808b96ec7de194532ecb6ad875146
Jarak Kritkiattisak <admin@mycools.in.th>
2018-09-04 20:03:44 +0700

    aaa

--- resources/views/frontend/includes/meta.blade.php
+++ resources/views/frontend/includes/meta.blade.php
@@ -45,3 +45,12 @@
 
   gtag('config', 'UA-125168373-1');
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109066356-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109066356-1');
+</script>
