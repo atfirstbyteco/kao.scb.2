330e8b39e257959501043e7af4b0147f99a28d95
AJENAJEN <i_hear_nie@hotmail.com>
2018-09-08 12:31:09 +0700

    add cache file

--- resources/views/frontend/includes/stylesheet.blade.php
+++ resources/views/frontend/includes/stylesheet.blade.php
@@ -2,4 +2,4 @@
 <link rel="stylesheet" type="text/css" href="/fonts/afb.helvetica.css" />
 <link rel="stylesheet" type="text/css" href="/vendors/OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css" />
 <link rel="stylesheet" type="text/css" href="/vendors/custom-scrollbar/jquery.mCustomScrollbar.css" />
-<link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css?v=1.2') }}" />
+<link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}" />
